8b104eb4a1e0af824be30ca582a0fec267956c69
SteveMustanski <steve.mustanski@gmail.com>
2023-05-28 14:29:55 -0400

    added the rest of crud to fastify service

--- my-service/model.js
+++ my-service/model.js
@@ -11,7 +11,7 @@ function bicycleModel () {
     2: { brand: 'Batavus', color: 'yellow' }
   }
   return {
-    read
+    create, read, update, del, uid
   }
 
   function read (id, cb) {
@@ -21,5 +21,52 @@ function bicycleModel () {
       return
     }
     setImmediate(() => cb(null, db[id]))
+  };
+
+  function uid () {
+    return Object.keys(db)
+      .sort((a,b) => a - b)
+      .map(Number)
+      .filter(n => !isNaN(n))
+      .pop() + 1 + ''
+  }
+
+  function create (id, data, cb) {
+    if (db.hasOwnProperty(id)) {
+      const err = Error('resource exists');
+      setImmediate(() => cb(err))
+      return
+    }
+    db[id] = data
+    setImmediate(() => cb(null, id));
+  }
+
+  function read (id, cb) {
+    if (!(db.hasOwnProperty(id))) {
+      const err = Error('not found')
+      setImmediate(() => cb(err))
+      return
+    }
+    setImmediate(() => cb(null, db[id]))
+  }
+
+  function update (id, data, cb) {
+    if (!(db.hasOwnProperty(id))) {
+      const err = Error('not found')
+      setImmediate(() => cb(err))
+      return
+    }
+    db[id] = data
+    setImmediate(() => cb())
+  }
+
+  function del (id, cb) {
+    if (!(db.hasOwnProperty(id))) {
+      const err = Error('not found')
+      setImmediate(() => cb(err))
+      return
+    }
+    delete db[id]
+    setImmediate(() => cb())
   }
 }
